39d263a89811941baf2f0f51decbb93f5e67afcc
mrtng1 <mrtngrulyo@gmail.com>
2023-02-03 12:22:39 +0100

    added app icon

--- src/dk/easv/Main.java
+++ src/dk/easv/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -39,6 +40,8 @@ public class Main extends Application {
         Scene scene = new Scene(root);
         scene.setFill(Color.TRANSPARENT);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.getIcons().add(new Image("dk/easv/presentation/view/images/loginIcon.png"));
+
 
         movableWindow(scene, primaryStage);
 
