a39d07e7745725e3c1459e8f19a519b964a26ce2
Tomáš Dračka <tomidracka@gmail.com>
2023-02-22 09:16:03 +0100

    Fetching movie images

--- src/dk/easv/presentation/controller/MainController.java
+++ src/dk/easv/presentation/controller/MainController.java
@@ -6,12 +6,12 @@ import dk.easv.entities.*;
 import dk.easv.entities.api.Result;
 import dk.easv.presentation.model.MainModel;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import dk.easv.entities.api.TMDB;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -23,6 +23,8 @@ import java.net.URL;
 import java.util.*;
 
 public class MainController implements Initializable {
+    @FXML
+    private BorderPane rootLayout;
     @FXML
     private Label nameLabel;
     @FXML
@@ -37,24 +39,89 @@ public class MainController implements Initializable {
 
     public void setUsername(String username) {
         this.username = username;
-
         nameLabel.setText(username);
     }
 
     public void setModel(MainModel model) {
         this.model = model;
 
-        // Loading users...
-        startTimer("Load users");
-        model.loadUsers();
-        stopTimer();
-
         // Information about the user
         getLoggedUser();
 
         // Getting top movies from similar people to our USER
-        model.getTopMoviesFromSimilarPeople(user);
-        System.out.println(model.getTopMoviesFromSimilarPeople(user));
+        startTimer("Loading movies");
+        loopMovies();
+        stopTimer();
+    }
+
+    public void loopMovies() {
+        List<TopMovie> movieTitles = model.getTopMoviesFromSimilarPeople(user);
+
+        int limit = 10;
+        int counter = 0;
+
+        for (TopMovie movieTitle : movieTitles) {
+            if (counter == limit) {
+                break;
+            }
+
+            try {
+                String query = movieTitle.getTitle();
+
+                String apiKey="46e91ce5acfdab6d23d26f340d638a2d";
+                String imagePath="https://image.tmdb.org/t/p/w400/";
+                String uri =
+                        "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey +
+                                "&language=en-US&query=" + query +
+                                "&page=1&include_adult=true";
+
+                URL url = new URL(uri);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Accept", "application/json");
+
+                if (conn.getResponseCode() != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : "
+                            + conn.getResponseCode());
+                }
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        (conn.getInputStream())));
+
+                try(Reader reader = new BufferedReader(new InputStreamReader(
+                        (conn.getInputStream())))){
+
+                    Gson gson = new GsonBuilder().create();
+                    TMDB p = gson.fromJson(reader, TMDB.class);
+
+                    List<Result> results = p.getResults();
+                    if (results.isEmpty()){
+                        System.out.println("No results found for movie: " + query);
+                    }
+                    else {
+                        Result r = results.isEmpty() ? new Result() : results.get(0);
+
+                        if (imgPoster!=null) {
+                            imgPoster.imageProperty().setValue(new Image(imagePath + r.getPoster_path()));
+                        }
+
+                        System.out.println(imagePath + r.getPoster_path());
+                    }
+                }
+                conn.disconnect();
+
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+            //System.out.println(movieTitle.getTitle());
+            counter++;
+        }
     }
 
     public void getLoggedUser () {
@@ -73,62 +140,6 @@ public class MainController implements Initializable {
             System.out.println("User found: " + user);
         } else {
             System.out.println("User not found.");
-        }
-    }
-
-
-    public void apiSet() {
-        TopMovie movie = model.getObsTopMoviesSimilarUsers().get(0);
-        System.out.println(movie.getMovie());
-
-        try {
-            String query = " ";
-            String apiKey="46e91ce5acfdab6d23d26f340d638a2d";
-            String imagePath="https://image.tmdb.org/t/p/w400/";
-            String uri =
-                    "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey +
-                            "&language=en-US&query=" + query +
-                            "&page=1&include_adult=false";
-
-            URL apiUrl = new URL(uri);
-            HttpURLConnection conn = (HttpURLConnection) apiUrl.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
-
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + conn.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (conn.getInputStream())));
-
-            try(Reader reader = new BufferedReader(new InputStreamReader(
-                    (conn.getInputStream())))){
-                Gson gson = new GsonBuilder().create();
-                TMDB p = gson.fromJson(reader, TMDB.class);
-                List<Result> results = p.getResults();
-                Result r = results.isEmpty() ? new Result() : results.get(0);
-
-                FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("dk/easv/presentation/view/card.fxml"));
-                hb = fxmlLoader.load();
-                if (r.getPoster_path()==null){
-                    System.out.println("image not found");
-                }
-                else {
-                    imgPoster.imageProperty().setValue(new Image(imagePath + r.getPoster_path()));
-                    System.out.println(imagePath + r.getPoster_path());
-                }
-            }
-            conn.disconnect();
-        } catch (MalformedURLException e) {
-
-            e.printStackTrace();
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
         }
     }
 
